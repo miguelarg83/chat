baaea2839b3580e3f9ac9c957fe0572849e82d3d
miguelarg83 <41567483+miguelarg83@users.noreply.github.com>
2022-02-28 16:35:28 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,28 +15,9 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/',[App\Http\Controllers\WelcomeController::class,'index'])->name('welcome')->middleware('verified'); // index
 
 Auth::routes(['verify'=>true]);
 
-// Chat
-// Route::get('/chat',[App\Http\Controllers\ChatController::class, 'index'])->name('chat')->middleware('verified');
-// Chat
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
-
-
-// Route::get('/pruebas', [App\Http\Controllers\PruebaController::class, 'index']);
-// Route::post('/pruebas', [App\Http\Controllers\PruebaController::class, 'index'])->name('store');
-
-// tabla-data-tabla
-// Route::view('/data-table', 'productos/data-table')->name('productos.data-table');
-// Botones de la tabla para editar
-// Route::view('/producto','productos.create')->name('productos.create');
-// Route::view('/producto/{id}','productos.edit')->name('producto.edit');
-// tabla-data-tabla
 
